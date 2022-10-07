9a640890f6745ed2fce7a058dbd2d4649a8323b0
Dharanendran <103572202+Dharanendran@users.noreply.github.com>
2022-10-07 18:48:31 +0530

    Create Assignment 3 ( Dharanendran A).py

---
+++ Assignments/Dharanendran_A/Assignment 3 ( Dharanendran A).py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+ 
+ledPin = 22
+ 
+def setup():
+        GPIO.setmode(GPIO.BOARD) # GPIO Numbering of Pins
+        GPIO.setup(ledPin, GPIO.OUT) # Set ledPin as output
+        GPIO.output(ledPin, GPIO.LOW) # Set ledPin to LOW to turn Off the LED
+ 
+def loop():
+        while True:
+                print 'LED on'
+                GPIO.output(ledPin, GPIO.HIGH) # LED On
+                time.sleep(1.0) # wait 1 sec
+                print 'LED off'
+                GPIO.output(ledPin, GPIO.LOW) # LED Off
+                time.sleep(1.0) # wait 1 sec
+def endprogram():
+ 
+        GPIO.output(ledPin, GPIO.LOW) # LED Off
+        GPIO.cleanup() # Release resources
+ 
+if _name_ == '_main_':
+        setup()
+        try:
+                loop()
+        except KeyboardInterrupt:
+                endprogram()
